3e9a90ce4e5f64d3d4b7f57a89e814a83adc1232
minhnhattrinh312 <minhnhattrinh312@gmail.com>
2024-06-13 02:19:50 +0700

    update new version

--- torch_radon24/radon.py
+++ torch_radon24/radon.py
@@ -60,37 +60,74 @@ class Radon(torch.nn.Module):
         Returns:
             out (torch.tensor, (bzs, 1, W, angles)): sinogram
         """
-        bsz, _, shape_size, _ = image.shape
+        bsz, _, W, H = image.shape
+
+        # Store original shape and padding/cropping info
+        self.resize_info = {"original_shape": (W, H), "pad": False, "crop": False}
+
+        if W != self.image_size or H != self.image_size:
+            diff_W = self.image_size - W
+            diff_H = self.image_size - H
+            if diff_W > 0 or diff_H > 0:
+                # Padding if the image is smaller than self.image_size
+                pad_W = max(diff_W // 2, 0)
+                pad_H = max(diff_H // 2, 0)
+                image = torch.nn.functional.pad(image, (pad_H, pad_H, pad_W, pad_W), mode="constant", value=0)
+                self.resize_info["pad"] = (pad_W, pad_H)
+            else:
+                # Center crop if the image is larger than self.image_size
+
+                crop_W = W - self.image_size
+                crop_H = H - self.image_size
+                crop_W_start = crop_W // 2
+                crop_H_start = crop_H // 2
+                image = image[:, :, crop_W_start : crop_W_start + self.image_size, crop_H_start : crop_H_start + self.image_size]
+                self.resize_info["crop"] = (crop_W_start, crop_H_start)
+
         out_fl = torch.nn.functional.grid_sample(image, self.rotated.repeat(bsz, 1, 1, 1), align_corners=True)
-        out_fl = out_fl.reshape(bsz, 1, self.n_angles, shape_size, shape_size)
+        out_fl = out_fl.reshape(bsz, 1, self.n_angles, self.image_size, self.image_size)
         out = out_fl.sum(3).permute(0, 1, 3, 2)
         return out
 
     def filter_backprojection(self, input):
-        """Apply (filtered) backprojection on input sinogramm.
+        """Apply (filtered) backprojection on input sinogram.
 
         Args:
-            image (torch.tensor, (bzs, 1, W, angles)): sinogramm
+            input (torch.tensor, (bzs, 1, W, angles)): sinogram
 
         Returns:
             out (torch.tensor, (bzs, 1, W, H)): reconstructed image
         """
         bsz, _, det_count, _ = input.shape
         if self.filter is not None:
-            # pad input
+            # Pad input
             padded_input = torch.nn.functional.pad(input, [0, 0, 0, self.pad_width], mode="constant", value=0)
-            # apply filter
+            # Apply filter
             projection = torch.fft.fft(padded_input, dim=2) * self.filter[:, None]
             radon_filtered = torch.real(torch.fft.ifft(projection, dim=2))[:, :, :det_count, :]
         else:
             radon_filtered = input
-        # reconstruct
+        # Reconstruct
         grid = self.grid.repeat(bsz, 1, 1, 1)
         reconstructed = torch.nn.functional.grid_sample(radon_filtered, grid, mode="bilinear", padding_mode="zeros", align_corners=True)
         reconstructed = reconstructed.view(bsz, self.n_angles, 1, self.image_size, self.image_size).sum(1)
         reconstructed = reconstructed / self.step_size
-        # circle
+        # Circle
         if self.circle:
             reconstructed_circle = self.reconstruction_circle.repeat(bsz, 1, 1, 1)
             reconstructed[reconstructed_circle == 0] = 0.0
-        return reconstructed * np.pi / (2 * self.n_angles)
+
+        reconstructed = reconstructed * np.pi / (2 * self.n_angles)
+
+        # Resize the reconstructed image back to the original shape
+        original_shape = self.resize_info["original_shape"]
+        if self.resize_info["pad"]:
+            pad_W, pad_H = self.resize_info["pad"]
+            reconstructed = reconstructed[:, :, pad_W : pad_W + original_shape[0], pad_H : pad_H + original_shape[1]]
+        elif self.resize_info["crop"]:
+            # crop_W_start, crop_H_start = self.resize_info['crop']
+            pad_W = (original_shape[0] - self.image_size) // 2
+            pad_H = (original_shape[1] - self.image_size) // 2
+            reconstructed = torch.nn.functional.pad(reconstructed, (pad_H, pad_H, pad_W, pad_W), mode="constant", value=0)
+
+        return reconstructed
