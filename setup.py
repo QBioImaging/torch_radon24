8a7a390ff90b3375cb076a645d5e7c531c93a94c
minhnhattrinh312 <minhnhattrinh312@gmail.com>
2024-07-08 14:04:51 +0100

    build version 1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="torch_radon24",
-    version="0.9",
+    version="1.0",
     description="Radon Transformation for Pytorch 2.0 package",
     author="Minh Nhat Trinh",
     license="GNU GENERAL PUBLIC LICENSE",
